14dd6b52e07c5a446684fc0fbce31feb08c28ddf
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-06-07 15:30:13 +0200

    footer add

---
+++ src/footer/footer.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import './footer.css';
+
+export default function Footer(){
+    return (
+    <footer>
+         <p>© ESCCI Compagny</p>
+     </footer>) 
+}
+
+
+export default Footer;
