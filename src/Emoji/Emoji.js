d2f1c5a9bff305e09e7f4e603f7bcef56ecc3a70
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-06-08 09:31:01 +0200

    add emoji css

--- src/Emoji/Emoji.js
+++ src/Emoji/Emoji.js
@@ -1,13 +1,30 @@
-// import './Emoji.css'
+import './Emoji.css'
 
 export default function Emojis(){
     return(
+        <div className="emojis">
         <ul>
             <li>
                <button>
-                   <span>😀</span>
+                   <span id="smile">😀</span>
                </button>
             </li>
         </ul>
+        <ul>
+            <li>
+               <button>
+                   <span id="sad">😥</span>
+               </button>
+            </li>
+        </ul>
+        <ul>
+            <li>
+               <button>
+                   <span id="tired">🥱</span>
+               </button>
+            </li>
+        </ul>
+        </div>
+        
     )
 }
