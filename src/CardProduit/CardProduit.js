ea2e02f3e8b57eea729b8675010853bb6cbaf343
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-06-09 09:39:30 +0200

    add originals productus

--- src/CardProduit/CardProduit.js
+++ src/CardProduit/CardProduit.js
@@ -7,7 +7,8 @@ export default function CardProduit(props) {
     return (
         
         <figure>
-            <p>{props.image}</p>
+            <img src={props.image} />
+            {/* <p>{props.image}</p> */}
             <h3>{props.titre}</h3>
             <p>{props.description}</p>
             <h4>{props.prix}</h4>
